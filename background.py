b671f00c4bbaaf6e98d11dbcfa764a0bfbf9a001
AKASH KUMAR <33580834+ashky23@users.noreply.github.com>
2021-06-21 22:29:14 +0530

    added comments

--- background.py
+++ background.py
@@ -1,3 +1,5 @@
+# This module is used to capture your background
+
 import cv2
 
 cap = cv2.VideoCapture(0)
@@ -7,7 +9,8 @@ while cap.isOpened():
     if ret:
         cv2.imshow("image", back)
         if cv2.waitKey(5) == ord('q'):
-            print(ord('q'))
+#             print(ord('q'))
+#saving the background image
             cv2.imwrite('image.jpg', back)
             break
 
